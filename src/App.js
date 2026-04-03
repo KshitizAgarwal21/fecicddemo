06dc63c2ce0f4cd7ab80d5779f6e394d9224051f
KshitizAgarwal21 <kshitizagarwal27@gmail.com>
2026-04-03 15:56:49 +0530

    Demo app 12 added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to Devops with AWS
+          Welcome to Devops with AWS12
         </a>
       </header>
     </div>
